4966fefd1e399dd48bdf308ee455b73d52569269
virginiafreitas <virginiacdefreitas@gmail.com>
2023-11-23 20:39:35 -0800

    added new projects to portfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -7,97 +7,140 @@
 
           <div className="row row-cols-3 g-4 mb-4">
 
-          <div className="col">
-            <div className="card">
-              <img src="../../images/widget-wizard.png" class="card-img-top" alt="..."></img>
-              <div className="card-body">
-              <h5 className="card-title">Widget Wizard</h5>
-              <a href="https://tranquil-tor-75275-57652b10176e.herokuapp.com/" target="_blank" className="card-link">Deployed Website</a>
-              <a href="https://github.com/Popiuy/WidgetWizard" target="_blank" className="card-link">GitHub Repo</a>
+            <div className="col">
+              <div className="card">
+                <img src="../../images/widget-wizard.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Widget Wizard</h5>
+                <a href="https://tranquil-tor-75275-57652b10176e.herokuapp.com/" target="_blank" className="card-link">Deployed Website</a>
+                <a href="https://github.com/Popiuy/WidgetWizard" target="_blank" className="card-link">GitHub Repo</a>
+                </div>
               </div>
-            </div>
             </div>
 
             <div className="col">
-            <div className="card">
-              <img src="../../images/surf-report.png" class="card-img-top" alt="..."></img>
-              <div className="card-body">
-              <h5 className="card-title">Surf Report</h5>
-              <a href="https://surf-report-app-7b1dc82c323b.herokuapp.com/" target="_blank" className="card-link">Deployed Website</a>
-              <a href="https://github.com/Lopez-Jordan/Surf-Report" target="_blank" className="card-link">GitHub Repo</a>
+              <div className="card">
+                <img src="../../images/surf-report.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Surf Report</h5>
+                <a href="https://surf-report-app-7b1dc82c323b.herokuapp.com/" target="_blank" className="card-link">Deployed Website</a>
+                <a href="https://github.com/Lopez-Jordan/Surf-Report" target="_blank" className="card-link">GitHub Repo</a>
+                </div>
               </div>
-            </div>
             </div>
 
             <div className="col">
-            <div className="card">
-              <img src="../../images/bookflix.png" class="card-img-top" alt="..."></img>
-              <div className="card-body">
-              <h5 className="card-title">BookFlix</h5>
-              <a href="https://gsr142.github.io/bookFlix/" target="_blank" className="card-link">Website</a>
-              <a href="https://github.com/gsr142/bookFlix" target="_blank" className="card-link">GitHub</a>
+              <div className="card">
+                <img src="../../images/bookflix.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">BookFlix</h5>
+                <a href="https://gsr142.github.io/bookFlix/" target="_blank" className="card-link">Website</a>
+                <a href="https://github.com/gsr142/bookFlix" target="_blank" className="card-link">GitHub</a>
+                </div>
               </div>
             </div>
-            </div>
-
 
           </div>
         
 
           <div className="row row-cols-3 g-4 mb-4">
 
-          <div className="col">
-            <div className="card">
-              <img src="../../images/weather-dashboard.png" class="card-img-top" alt="..."></img>
-              <div className="card-body">
-              <h5 className="card-title">Weather Forecast</h5>
-              <a href="https://virginiafreitas.github.io/weather-dashboard/" target="_blank" className="card-link">Website</a>
-              <a href="https://github.com/virginiafreitas/weather-dashboard" target="_blank" className="card-link">GitHub</a>
+            <div className="col">
+              <div className="card">
+                <img src="../../images/weather-dashboard.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Weather Forecast</h5>
+                <a href="https://virginiafreitas.github.io/weather-dashboard/" target="_blank" className="card-link">Website</a>
+                <a href="https://github.com/virginiafreitas/weather-dashboard" target="_blank" className="card-link">GitHub</a>
+                </div>
               </div>
-            </div>
             </div>
 
             <div className="col">
-            <div className="card">
-              <img src="../../images/code-quiz.png" class="card-img-top" alt="..."></img>
-              <div className="card-body">
-              <h5 className="card-title">Code Quiz</h5>
-              <a href="https://virginiafreitas.github.io/code-quiz/" target="_blank" className="card-link">Deployed Website</a>
-              <a href="https://github.com/virginiafreitas/code-quiz" target="_blank" className="card-link">GitHub Repo</a>
+              <div className="card">
+                <img src="../../images/code-quiz.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Code Quiz</h5>
+                <a href="https://virginiafreitas.github.io/code-quiz/" target="_blank" className="card-link">Deployed Website</a>
+                <a href="https://github.com/virginiafreitas/code-quiz" target="_blank" className="card-link">GitHub Repo</a>
+                </div>
               </div>
-            </div>
             </div>
 
             <div className="col">
-            <div className="card">
-              <img src="../../images/password-generator.png" class="card-img-top" alt="..."></img>
-              <div className="card-body">
-              <h5 className="card-title">Password Generator</h5>
-              <a href="https://virginiafreitas.github.io/password-generator/" target="_blank" className="card-link">Website</a>
-              <a href="https://github.com/virginiafreitas/password-generator" target="_blank" className="card-link">GitHub</a>
+              <div className="card">
+                <img src="../../images/password-generator.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Password Generator</h5>
+                <a href="https://virginiafreitas.github.io/password-generator/" target="_blank" className="card-link">Website</a>
+                <a href="https://github.com/virginiafreitas/password-generator" target="_blank" className="card-link">GitHub</a>
+                </div>
               </div>
             </div>
-            </div>
-
 
           </div>
-
 
           <div className="row row-cols-3 g-4 mb-4">
 
-          <div className="col">
-            <div className="card">
-              <img src="../../images/workday-schedule.png" class="card-img-top" alt="..."></img>
-              <div className="card-body">
-              <h5 className="card-title">Workday Schedule</h5>
-              <a href="https://virginiafreitas.github.io/workday-schedule/" target="_blank" className="card-link">Website</a>
-              <a href="https://github.com/virginiafreitas/workday-schedule" target="_blank" className="card-link">GitHub</a>
+            <div className="col">
+              <div className="card">
+                <img src="../../images/react-portfolio.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Portfolio React.js</h5>
+                <a href="https://main--luxury-parfait-825418.netlify.app/" target="_blank" className="card-link">Website</a>
+                <a href="https://github.com/virginiafreitas/react-portfolio" target="_blank" className="card-link">GitHub</a>
+                </div>
               </div>
             </div>
+
+            <div className="col">
+              <div className="card">
+                <img src="../../images/ecommerce-backend.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">E-commerce Backend</h5>
+                <a href="https://github.com/virginiafreitas/ecommerce-backend" target="_blank" className="card-link">GitHub</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card">
+                <img src="../../images/Horiseon.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Social Network Backend</h5>
+                <a href="https://virginiafreitas.github.io/code-refactor-challenge/" target="_blank" className="card-link">Website</a>
+                <a href="https://drive.google.com/file/d/1pqqa-XCoU99IAJY0qNJvDcsDO7AThHPK/view" target="_blank" className="card-link">Demo</a>
+                </div>
+              </div>
             </div>
 
           </div>
+          
+          <div className="row row-cols-3 g-4 mb-4">
 
+            <div className="col">
+              <div className="card">
+                <img src="../../images/workday-schedule.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Workday Schedule</h5>
+                <a href="https://virginiafreitas.github.io/workday-schedule/" target="_blank" className="card-link">Website</a>
+                <a href="https://github.com/virginiafreitas/workday-schedule" target="_blank" className="card-link">GitHub</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card">
+                <img src="../../images/Horiseon.png" class="card-img-top" alt="..."></img>
+                <div className="card-body">
+                <h5 className="card-title">Refactor: accessibility & SEO</h5>
+                <a href="https://virginiafreitas.github.io/code-refactor-challenge/" target="_blank" className="card-link">Website</a>
+                <a href="https://github.com/virginiafreitas/code-refactor-challenge" target="_blank" className="card-link">GitHub</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
 
 
         </div>
